1f30852a39c3b0aec345f08447337b9858eae9cb
uqingu <uqingu@foxmail.com>
2022-05-22 21:02:24 +0800

    hello-actix-web

---
+++ hello-actix-web/src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{get, web, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyObj {
+    name: String,
+}
+
+#[get("/a/{name}")]
+async fn index(name: web::Path<String>) -> Result<impl Responder> {
+    let obj = MyObj {
+        name: name.to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{App, HttpServer};
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
